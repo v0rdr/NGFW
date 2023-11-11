bc5f1cbf6c30deaf43627fb73dc99c646462e9fe
v0rdr <146991765+v0rdr@users.noreply.github.com>
2023-11-11 17:22:42 -0500

    cleanup

--- blockips.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-#BLOCK BOWTIEDCYBER.XYZ!!!
-iptables -t raw -A PREROUTING -s 178.128.237.187 -j DROP
-#BLOCK GOOGLE!!!
-iptables -t raw -A PREROUTING -s 142.251.41.46 -j DROP
-#and also google haha
-iptables -t raw -A PREROUTING -s 142.251.32.78 -j DROP
